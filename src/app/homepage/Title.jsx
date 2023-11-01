542c2e9fd52eb32361a60478f7e0267f3187e01c
Anastassia Vomm <anastassia.vomm@gmail.com>
2023-11-01 13:13:03 +0200

    background fix

--- src/app/homepage/Title.jsx
+++ src/app/homepage/Title.jsx
@@ -4,10 +4,13 @@ import styles from '../homepage/page.module.css';
 
 export const Title = () => {
   return (
-    <AnimatedTitle>
-      THE BEST <span className={styles.green_text}>PLATFORM</span> FOR{' '}
-      <span className={styles.green_text}>COLLABORATION</span> BETWEEN <br />
-      BLOGGERS & BRANDS
-    </AnimatedTitle>
+    <>
+      <AnimatedTitle>
+        THE BEST <span className={styles.green_text}>PLATFORM</span> FOR{' '}
+        <span className={styles.green_text}>COLLABORATION</span> BETWEEN <br />
+        BLOGGERS & BRANDS
+      </AnimatedTitle>
+      <div className={styles.light__spot_outer}></div>
+    </>
   );
 };
