72c910766891d0e7939aff100322061fd732ca0d
Anastassia Vomm <anastassia.vomm@gmail.com>
2023-12-18 20:29:34 +0200

    filter and sort added to table

--- src/app/(dashboard)/components/ActiveProjects/ActiveProjects.jsx
+++ src/app/(dashboard)/components/ActiveProjects/ActiveProjects.jsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useState } from 'react';
+
 import { projects } from '@/mockData/projects';
 
 import { ProjectsTable } from '../ProjectsTable/ProjectsTable';
@@ -8,12 +12,27 @@ import { montserrat } from '@/utils/fonts';
 export const ActiveProjects = () => {
   const activeProjects = projects.filter(project => project.status === 'active');
 
+  const [_, setSearchText] = useState('');
+
+  const handleSearch = (selectedKeys, confirm) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+  };
+  const handleReset = clearFilters => {
+    clearFilters();
+    setSearchText('');
+  };
+
   return (
     <>
       <h3 className={`${personalStyles.heading} ${montserrat.className} profile-heading`}>
         In Progress
       </h3>
-      <ProjectsTable projects={activeProjects} />
+      <ProjectsTable
+        projects={activeProjects}
+        handleSearch={handleSearch}
+        handleReset={handleReset}
+      />
     </>
   );
 };
