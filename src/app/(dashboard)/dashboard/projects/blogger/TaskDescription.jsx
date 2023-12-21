c7afd5cdd87f2c330976903dfd85e7551d9223b7
Anastassia Vomm <anastassia.vomm@gmail.com>
2023-12-21 17:24:28 +0200

    additions to tasks modal

--- src/app/(dashboard)/dashboard/projects/blogger/TaskDescription.jsx
+++ src/app/(dashboard)/dashboard/projects/blogger/TaskDescription.jsx
@@ -2,9 +2,9 @@
 
 import { useState, useEffect } from 'react';
 
-import { Form, Row, Col, Input, DatePicker, Tooltip, Button, Popover } from 'antd';
+import { Form, Row, Col, Input, DatePicker, Tooltip, Button, Popover, Image, Space } from 'antd';
 
-import { CopyOutlined } from '@ant-design/icons';
+import { CopyOutlined, DownloadOutlined, ZoomInOutlined, ZoomOutOutlined } from '@ant-design/icons';
 
 import dayjs from 'dayjs';
 
@@ -12,7 +12,7 @@ import styles from '../projects.module.css';
 import formStyles from '../../profile/personal-info/personalInfo.module.css';
 import { montserrat } from '@/utils/fonts';
 
-export const TaskDescription = ({ task }) => {
+export const TaskDescription = ({ task, project }) => {
   const [form] = Form.useForm();
   const [valuesForm, setValuesForm] = useState({ ...task });
 
@@ -25,17 +25,66 @@ export const TaskDescription = ({ task }) => {
           dayjs(task.periodOfExecution.split(' - ')[1], dateFormat),
         ])
       : null;
+    form.setFieldValue('promo', project.promoCode);
+    form.setFieldValue('link-subscribers', project.linkForSubscribers);
+    form.setFieldValue('payment-type', project.paymentType);
   }, [task, form]);
 
   const { RangePicker } = DatePicker;
   const dateFormat = 'YYYY/MM/DD';
+
+  const onPosterDownload = () => {
+    fetch(project.poster)
+      .then(response => response.blob())
+      .then(blob => {
+        const url = URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'image.png';
+        document.body.appendChild(link);
+        link.click();
+        URL.revokeObjectURL(url);
+        link.remove();
+      });
+  };
 
   return (
     <div className={styles.rightContainer}>
       {task && (
         <Form layout="vertical" form={form} onValuesChange={v => setValuesForm(v)}>
           <Row gutter={{ xs: 10, md: 15, xl: 30 }}>
-            <Col span={24} style={{ marginBottom: '35px' }}>
+            <Col
+              className={project.poster ? styles.posterWrap : null}
+              span={24}
+              style={{ marginBottom: '35px' }}
+            >
+              {project.poster && (
+                <Image
+                  className={styles.poster}
+                  width={250}
+                  src={project.poster}
+                  preview={{
+                    toolbarRender: (
+                      _,
+                      { transform: { scale }, actions: { onZoomOut, onZoomIn } }
+                    ) => (
+                      <Space size={12} className="toolbar-wrapper">
+                        <DownloadOutlined style={{ fontSize: '200%' }} onClick={onPosterDownload} />
+                        <ZoomOutOutlined
+                          style={{ fontSize: '200%' }}
+                          disabled={scale === 1}
+                          onClick={onZoomOut}
+                        />
+                        <ZoomInOutlined
+                          style={{ fontSize: '200%' }}
+                          disabled={scale === 50}
+                          onClick={onZoomIn}
+                        />
+                      </Space>
+                    ),
+                  }}
+                />
+              )}
               <p className={`${styles.smallText} text`}>{valuesForm.description}</p>
             </Col>
             <Col xs={24} md={7}>
@@ -114,7 +163,7 @@ export const TaskDescription = ({ task }) => {
                             </p>
                           </div>
                         }
-                        trigger={'click'}
+                        trigger={task.hashtags !== '' ? 'click' : null}
                       >
                         <Button
                           size="small"
@@ -144,6 +193,108 @@ export const TaskDescription = ({ task }) => {
                   </p>
                 }
                 name="wordsToUse"
+              >
+                <Input disabled className={`${formStyles.input} profile-input`} />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={7}>
+              <Form.Item
+                label={
+                  <p className={`${formStyles.label} ${montserrat.className} profile-input-label`}>
+                    Promo code
+                  </p>
+                }
+                name="promo"
+              >
+                <Input
+                  disabled
+                  className={`${formStyles.input} profile-input text`}
+                  suffix={
+                    <Tooltip title={'Copy to clipboard'}>
+                      <Popover
+                        color="#9b9c86"
+                        placement="right"
+                        content={
+                          <div>
+                            <p className={`${styles.popoverText} ${montserrat.className}`}>
+                              Copied to clipboard!
+                            </p>
+                          </div>
+                        }
+                        trigger={project.promoCode !== '' ? 'click' : null}
+                      >
+                        <Button
+                          size="small"
+                          onClick={() => {
+                            navigator.clipboard.writeText(project.promoCode);
+                          }}
+                          type="text"
+                          icon={
+                            <CopyOutlined
+                              className="icon-account-input"
+                              style={{ fontSize: '24px' }}
+                            />
+                          }
+                        />
+                      </Popover>
+                    </Tooltip>
+                  }
+                />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={15}>
+              <Form.Item
+                label={
+                  <p className={`${formStyles.label} ${montserrat.className} profile-input-label`}>
+                    Link for subscribers
+                  </p>
+                }
+                name="link-subscribers"
+              >
+                <Input
+                  disabled
+                  className={`${formStyles.input} profile-input text`}
+                  suffix={
+                    <Tooltip title={'Copy to clipboard'}>
+                      <Popover
+                        color="#9b9c86"
+                        placement="right"
+                        content={
+                          <div>
+                            <p className={`${styles.popoverText} ${montserrat.className}`}>
+                              Copied to clipboard!
+                            </p>
+                          </div>
+                        }
+                        trigger={project.linkForSubscribers !== '' ? 'click' : null}
+                      >
+                        <Button
+                          size="small"
+                          onClick={() => {
+                            navigator.clipboard.writeText(project.linkForSubscribers);
+                          }}
+                          type="text"
+                          icon={
+                            <CopyOutlined
+                              className="icon-account-input"
+                              style={{ fontSize: '24px' }}
+                            />
+                          }
+                        />
+                      </Popover>
+                    </Tooltip>
+                  }
+                />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={7}>
+              <Form.Item
+                label={
+                  <p className={`${formStyles.label} ${montserrat.className} profile-input-label`}>
+                    Payment type
+                  </p>
+                }
+                name="payment-type"
               >
                 <Input disabled className={`${formStyles.input} profile-input`} />
               </Form.Item>
