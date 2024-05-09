fc623217a0045246ffe767c254bb789e0db6e9f8
Yehor Skibnevskyi <105387497+Skyhor@users.noreply.github.com>
2024-05-09 12:52:04 +0200

    Additions to the RegErrorPage

--- src/app/(auth)/RegError/RegErrorPage.jsx
+++ src/app/(auth)/RegError/RegErrorPage.jsx
@@ -12,8 +12,18 @@ const RegErrorPage = () => {
 
   return (
     <div>
-      <h1 className={styles.error}>Error</h1>
-      <button onClick={handleClearLocalStorage}>OK :( </button>
+      <h1 className={styles.error}>Сталася помилка</h1>
+      <h2 className={styles.title}>Крок 1</h2>
+      <p className={styles.text}>
+        Якщо помилка сталася на кроку #1, перевірте правильність email, скоріше за все помилка тут.
+        Можливо ви вже зареєстровані, спробуйте перейти на сторінку Login
+      </p>
+      <h2 className={styles.title}>Крок 2</h2>
+      <p className={styles.text}>
+        Якщо помилка сталася на кроку #2, перевірте чи вірно ви обрали платформу, та переконайтесь
+        що у вас є 1000 підписників на ній
+      </p>
+      <button onClick={handleClearLocalStorage}>Повернутись на головну сторінку </button>
     </div>
   );
 };
